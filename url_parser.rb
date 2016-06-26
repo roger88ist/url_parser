4ad6a1f941324efe1922d6c98a9683653bc89edf
Rogerio Istiraneopulos <roger88ist@gmail.com>
2016-06-26 12:57:26 -0400

    added scheme and domain methods to make tests pass

--- url_parser.rb
+++ url_parser.rb
@@ -1,3 +1,21 @@
 class UrlParser
+	# attr_accessor :scheme
+	def initialize(uri)
+		@url = uri
+	end
+
+	def scheme
+		if @url.include?('https')
+			"https"
+		elsif @url.include?('http')
+			"http"
+		else
+			"not a valid url"
+		end	
+	end
+
+	def domain
+		@url.split('//')[1].split(':')[0]
+	end
 
 end
